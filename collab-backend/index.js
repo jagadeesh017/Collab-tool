3b4a4a063714649777030991226b675614d0e131
jagadeesh017 <jagadeesh.cse.17@gmail.com>
2025-07-12 23:43:51 +0530

    pkg-json updated

--- collab-backend/index.js
+++ collab-backend/index.js
@@ -12,11 +12,14 @@ const io = new Server(server, {
   }
 });
 const path =require('path');
-app.use(express.static(path.join(__dirname, '../client/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
 
+
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
 const PORT = process.env.PORT || 3000;
 let drawingData = [];
 let pdfData = null;
